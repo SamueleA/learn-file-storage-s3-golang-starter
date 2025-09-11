b3d75d2294da5a3c1cc2030511e95467d6b45796
samuelea <batonenbois@gmail.com>
2025-09-10 22:02:39 -0400

    s3 client

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -4,4 +4,8 @@ import (
 	"net/http"
 )
 
-func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {}
+func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
+	uploaderLimit := int64(1 << 30)
+	reader :=http.MaxBytesReader(w, r.Body, uploaderLimit)
+
+}
